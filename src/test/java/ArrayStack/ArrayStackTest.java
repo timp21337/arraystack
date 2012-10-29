ef63adf8605b9796588ddbc3241931e4c01c0a8f
timp21337 <timp@paneris.org>
2012-10-29 00:05:37 +0000

    Start to test constructors

--- src/test/java/ArrayStack/ArrayStackTest.java
+++ src/test/java/ArrayStack/ArrayStackTest.java
@@ -1,12 +1,54 @@
 package ArrayStack;
 
+import jdslcomp.simple.api.StackEmptyException;
+import junit.framework.TestCase;
+
 // JUnit 3
 
-public class ArrayStackTest {
+public class ArrayStackTest extends TestCase {
 	
+	
+	// Test constructor
+	
+	public void testConstructor() { 
+		try {
+  		new ArrayStack(-1);
+		} catch (NegativeArraySizeException e) { 
+		  e = null; // bug
+		}
+    ArrayStack zero = new ArrayStack(0);
+    try { 
+      zero.pop();
+    } catch (StackEmptyException e) { 
+      e = null;
+    }
+    try { 
+      zero.push(new Integer(1));
+    } catch (ArrayIndexOutOfBoundsException e) { 
+      e = null; //bug
+    }
+    try { 
+      zero.pop();
+    } catch (ArrayIndexOutOfBoundsException e) { 
+      e = null; //bug
+    }
+    try { 
+      zero.top();
+    } catch (ArrayIndexOutOfBoundsException e) { 
+      e = null; //bug
+    }
+    
+    ArrayStack one = new ArrayStack(1);
+    one.push("1");
+    try { 
+     one.push(new Integer(2));
+    } catch (ArrayIndexOutOfBoundsException e) { 
+      e = null; //bug
+    }
+	}
 	public void testPush() {
 		ArrayStack myStack = new ArrayStack(20);
-		myStack.push(2);
+		myStack.push(new Integer(2));
 	}
 
 }
